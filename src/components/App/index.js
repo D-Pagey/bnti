bedc469b75ca1ee675305393e8ce33e54b0c964f
D-Pagey <dan.page91@gmail.com>
2019-02-03 01:47:41 +0000

    chore: initialise
    [skip ci]

---
+++ src/components/App/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+    return (
+        <div className="App">
+            <h1>Bantiiiii</h1>
+            <p>API_GATEWAY = {process.env.REACT_APP_API_GATEWAY}</p>
+        </div>
+    );
+}
