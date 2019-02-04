8860b13accf962c9eb702a824f9c9ec001d63bbf
D-Pagey <dan.page91@gmail.com>
2019-02-04 23:55:40 +0000

    feat: adding pubg matches lamdba

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,5 @@
 const Chance = require('chance');
+const axios = require('axios');
 
 const chance = new Chance();
 
@@ -6,3 +7,16 @@ module.exports.hello = async () => ({
     statusCode: 200,
     body: JSON.stringify({ message: `Hello ${chance.name()}!` })
 });
+
+module.exports.pubgMatches = async () => {
+    const response = await axios({
+        method: 'get',
+        url: 'https://api.playbattlegrounds.com/shards/steam/matches/13dc7b6a-e39d-4a75-b1ad-d1841c70a42b',
+        headers: { Accept: 'application/vnd.api+json' }
+    });
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ message: response.data })
+    };
+};
