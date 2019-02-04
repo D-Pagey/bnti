7c07a18acafe2d60d4a0c7f694c1b5b0ed98395e
D-Pagey <dan.page91@gmail.com>
2019-02-04 23:26:54 +0000

    feat: add api data

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-export default function App() {
-    return (
-        <div className="App">
-            <h1>Bantiiiii</h1>
-            <p>API_GATEWAY = {process.env.REACT_APP_API_GATEWAY}</p>
-        </div>
-    );
+export default class App extends Component {
+    componentDidMount() {
+        axios.get('http://localhost:3001/pubg/matches').then((resp) => console.log('resp', resp));
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <h1>Bantiiiii</h1>
+                <p>API_GATEWAY = {process.env.REACT_APP_API_GATEWAY}</p>
+            </div>
+        );
+    }
 }
