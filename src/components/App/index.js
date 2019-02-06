1adb04a6e5ba4158d77f3a58075c760ca94fa75c
D-Pagey <dan.page91@gmail.com>
2019-02-06 01:16:21 +0000

    feat: adding basic routing

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Header from '../Header';
+import Chicken from '../Chicken';
 
 export default function App() {
     return (
-        <div className="App">
-            <h1>Bantiiiii</h1>
-            <p>API_GATEWAY = {process.env.REACT_APP_API_GATEWAY}</p>
-        </div>
+        <BrowserRouter>
+            <div className="App" style={{ maxWidth: 800, margin: '0 auto', fontFamily: 'sans-serif' }}>
+                <Header />
+                <Switch>
+                    <Route exact path="/" component={Chicken} />
+                    <Route path="/tasty" component={Chicken} />
+                    <Route path="/delicious" component={Chicken} />
+                </Switch>
+                <p>API_GATEWAY = {process.env.REACT_APP_API_GATEWAY}</p>
+            </div>
+        </BrowserRouter>
     );
 }
