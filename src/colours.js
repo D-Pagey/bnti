bf5fe3765b228b245b3588693e8b66c825f50232
Dan Page <dan.page91@gmail.com>
2019-02-15 22:30:52 +0000

    feat: add hero component
    [skip ci]

---
+++ src/colours.js
@@ -0,0 +1,4 @@
+export const colours = {
+    primaryColour: '#00A7CB',
+    black: '#353535'
+};
--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Hero from './components/Hero';
 
 export default function HomePage() {
     return (
-        <div>
+        <>
+            <Hero />
             <h1>Bantiiiii</h1>
             <p>API_GATEWAY = {process.env.REACT_APP_API_GATEWAY}</p>
             <Link to="/login">Go to login</Link>
-        </div>
+        </>
     );
 }
